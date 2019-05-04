a627abc48b0e2b88ee2e3d86d1eda44fb276dc65
Philippe <phillclesco@yahoo.com>
2019-05-04 02:54:56 -0400

    ?

--- c_addremove.c
+++ c_addremove.c
@@ -105,3 +105,10 @@ char *removeLine(char *content, char **line){
 
 }
 
+char *concat(const char *s1, const char *s2)
+{
+        char *result = (char *)malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+        strcpy(result, s1);
+        strcat(result, s2);
+        return result;
+}
