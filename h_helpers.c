4cc78a977940a5aeac4152e1d71755fcc987db1c
Philippe <phillclesco@yahoo.com>
2019-05-04 21:15:52 -0400

    Fixed a char append declaration

--- h_helpers.c
+++ h_helpers.c
@@ -1,19 +1,6 @@
 #include "c_client.h"
 #include "s_server.h"
 
-/**
-	* Adds char to the string that is being added to the codebook
-	* The way I implemented this is actually has the potential to create memory leaks.
-	* Thereason is because I can also pass a malloced string, and unless that is handled by the function that calles this, it will just abandon that original malloced string.
-	* If this isn't fixed I ran out of time.
-	*/
-char *charAppend(char str[], char charr){
-	char *newStr = malloc(strlen(str)+2);
-	strcpy(newStr, str);
-	newStr[strlen(str)] = charr;
-	newStr[strlen(str)+1] = '\0';
-	return newStr;
-}
 
 char *stringAppend(const char *str1, const char *str2, int len){
 	printf("string1: %d\n", strlen(str1));
