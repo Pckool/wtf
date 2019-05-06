36c1e325cb25335b6270c414c63e96eac6379515
Philippe <phillclesco@yahoo.com>
2019-05-06 09:18:23 -0400

    yeea

--- c_checkout.c
+++ c_checkout.c
@@ -70,7 +70,7 @@ void checkout(char *projectName, int sockfd){
 	printTokenLinks(msg_tokens);
 	ProtocolLink *currToken = (ProtocolLink *)malloc(sizeof(ProtocolLink));
 	currToken = currToken->next;
-	if(strcmp(msg_tokens->token, "file") == 0){ // if the first token is `file`
+	if(strcmp(currToken->token, "file") == 0){ // if the first token is `file`
 		memcpy(currToken, currToken->next, sizeof(ProtocolLink)); // go to the next link
 		printf("Recieved a file from the server of length %d!\n", strlen(message));
 		char *projName = currToken->token;
