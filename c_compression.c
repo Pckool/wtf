16926741142ba6297895a0a3e7729d0580b8f159
Philippe <philippe@defton-portable.ASPIRE>
2019-05-05 23:18:46 -0400

    YEET SHOULD FIX EVERYTHING

--- c_compression.c
+++ c_compression.c
@@ -5,13 +5,13 @@
 void sendTar(char *projectName){
     FILE *fp;
     int ret,len;
-    char buf[MAX] = {0};
+    char buf[2000000] = {0};
     fp = fopen("/home/priyanka/3455.tgz","rb");
-    while(len = fread(buf,1,1024,fp))
+    while(len = fread(buf, 1, 2000000, fp))
     //while((buf[0] = fgetc(fp)) != EOF)
     {
         printf("\nRead : %d",len);
-        ret = send(s,buf,strlen(buf),0);
+        ret = send(sockfd, buf, strlen(buf),0);
         printf("    Sent : %d",ret);
         if(ret == -1)
         {
