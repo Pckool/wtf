4f56e37a9e5831b8d7c8d6a89ab222560247a920
Philippe <phillclesco@yahoo.com>
2019-05-06 04:25:40 -0400

    added loading bar

--- c_checkout.c
+++ c_checkout.c
@@ -33,7 +33,15 @@ void checkout(char *projectName, int sockfd){
 	printf("waiting for server...\n");
 	// this will loop through until it recieves a readable responce from the socket.
 	while(waiting){
-		printf(". ");
+		printf("processing... |");
+		fflush(stdout);
+		// do something
+		printf("\b/");
+		fflush(stdout);
+		// do some more
+		printf("\b-");
+		fflush(stdout);
+		
 		if(ioctl(sockfd2, FIONREAD, &msg_length) == 0){
 			// printf("No data recieved from the server...\n");
 			continue;
