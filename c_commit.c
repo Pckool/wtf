8d9103eaa1e911ebc9e6324289690f28f801c569
RonJebara <rj396@scarletmail.rutgers.edu>
2019-05-06 23:48:59 -0400

    Im a sick fuck I live the vbuck

---
+++ c_commit.c
@@ -0,0 +1,31 @@
+#include "c_client.h"
+#include "h_both.h" 
+
+
+void commit_c(char* client, char* server){
+/*
+	DataLink *headcli = (DataLink *) malloc(sizeof(DataLink));
+	DataLink *headserv = (DataLink *)malloc(sizeof(DataLink));
+        int i = 0;
+        int x = 0;
+        while(client[i] != '\n'){
+                i++;
+        }
+        while (server[x] != '\n'){
+                x++;
+        }
+        if(x == i){
+                int val = strncmp(client, server, i);
+                if (val != 0){
+                        printf("%s\n", "Version numbers do not not match up.");
+                        exit(0);
+                }
+	}
+		tokenizeString(client, 10, headcli);
+		tokenizeString(server, 10, headserv);
+	while (headserv != NULL){
+		int k = strcmp(headserv->token, headcli->token);
+			if(k == 0)	
+*/
+}
+
