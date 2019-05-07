80a60f242179594a4c68d96eb98f23871ac1d5a8
Philippe G. Clesca <pgc42@scarletmail.rutgers.edu>
2019-05-06 21:29:10 -0400

    lol I fixed the segfault in tokenizer

--- h_helpers.c
+++ h_helpers.c
@@ -237,7 +237,7 @@ DataLink *tokenizeString(char *inputMsg, char separator, DataLink *head){
 
 		char frontLetter[2] = {copyToManipulate[0], '\0'};
 		
-		if( strcmp(frontLetter, separator) != 0 && strcmp(frontLetter,"\0") != 0){ // while we don't have the token separating char
+		if( strcmp(frontLetter, separatorStr) != 0 && strcmp(frontLetter, "\0") != 0){ // while we don't have the token separating char
 			char nextLetter[2] = {copyToManipulate[1], '\0'};
 			printf("\tAPPENDING STRING\n");
 			// append the char to the temp string
