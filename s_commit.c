c29290c1cf89878deeaa39e6b6b94dc2884e5511
Philippe <phillclesco@yahoo.com>
2019-05-08 00:22:29 -0400

    dasa

--- s_commit.c
+++ s_commit.c
@@ -146,7 +146,7 @@ void *pushFileToClient(void *dat){
         return;
     }
     printf("File sent successfully with %d/%d bytes written...\n", amm, fileStat.st_size);
-    
+    printf("Sending: %s...\n", project_buffer);
 
     close(fd_data);
 }
